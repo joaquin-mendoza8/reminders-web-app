5d5ea3f0f5cae5509e1cb294930fa5e46c90f58e
Joaquin Mendoza <joaquinmendoza707@gmail.com>
2023-05-24 12:34:59 -0700

    Created agnostic file paths

--- server.py
+++ server.py
@@ -5,6 +5,10 @@
 import http.server
 import socketserver
 import json
+import os
+
+# creates agnostic path to cwd (allows server to run on any machine)
+working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # list of reminders objects
 reminders = []
@@ -13,38 +17,37 @@ reminders = []
 class RequestHandler(http.server.BaseHTTPRequestHandler): # inherits from BaseHTTPRequestHandler
 
     # TODO: figure out how to replace absolute paths with relative paths
-    
+
     # override do_GET method to handle GET requests
     def do_GET(self):
         if self.path == '/api/reminders/index.html': # checks for index.html
             self.send_response(200)
             self.send_header('Content-Type', 'text/html')
             self.end_headers()
-            with open('/Users/joaquinmendoza/VSCode_Projects/CS-210/programs/web/index.html', 'rb') as file:
+            with open(working_dir + '/index.html', 'rb') as file:
                 self.wfile.write(file.read())
         elif self.path == '/api/reminders/styles.css': # checks for styles.css
             self.send_response(200)
             self.send_header('Content-Type', 'text/css')
             self.end_headers()
-            with open('/Users/joaquinmendoza/VSCode_Projects/CS-210/programs/web/styles.css', 'rb') as file:
+            with open(working_dir + '/styles.css', 'rb') as file:
                 self.wfile.write(file.read())
         elif self.path == '/api/reminders/script.js': # checks for scripts.js
             self.send_response(200)
             self.send_header('Content-Type', 'text/javascript')
             self.end_headers()
-            with open('/Users/joaquinmendoza/VSCode_Projects/CS-210/programs/web/script.js', 'rb') as file:
+            with open(working_dir + '/script.js', 'rb') as file:
                 self.wfile.write(file.read())
-        else: # checks other paths
-            if self.path == 'api/reminders':
-                self.send_response(200)
-                self.send_header('Content-Type', 'application/json')
-                self.end_headers()
-                self.wfile.write(bytes(json.dumps(reminders), 'utf-8'))
-            else: # not found
-                self.send_response(404) # send 404 status code
-                self.send_header('Content-Type', 'text/plain')
-                self.end_headers()
-                self.wfile.write(bytes('Not Found', 'utf-8'))
+        elif self.path == '/api/reminders': # checks for reminders database
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps(reminders), 'utf-8'))
+        else: # not found/invalid path
+            self.send_response(404) # send 404 status code
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes('Not Found', 'utf-8'))
 
     # TODO: override do_POST method to create new reminders
 
